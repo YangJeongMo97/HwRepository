e0523d05a50a9bd99d44ba20f9c811c45355fe78
y <y@192.168.219.101>
2022-06-19 12:48:55 +0900

    work in dev_C branch

--- MyGitJavaProj/src/A.java
+++ MyGitJavaProj/src/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is defualt A.java");
+		System.out.println("This line is created by dev_C branch(file A)");
 		B b = new B();
 		C c = new C();
 		b.callB();
